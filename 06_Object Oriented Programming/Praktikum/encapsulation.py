a4df0f013efeba86ff03f688893ff147ec720538
afrilis <afrilistihawa@gmail.com>
2024-03-06 02:55:57 +0700

    mengenai  oop

---
+++ 06_Object Oriented Programming/Praktikum/encapsulation.py
@@ -0,0 +1,31 @@
+# Contoh class
+class ClassRobot:
+    pass 
+
+# Instance
+class Cat:
+    def __init__(self, fur_color="No Name", num_of_leg=4):
+        self._fur_color = fur_color
+        self.num_of_leg = num_of_leg
+
+manis = Cat("Hitam", 4)
+pussy = Cat("Putih", 3)
+
+# Contoh dari type artibrute
+class Mobil:
+    def __init__(self, tipe, name, roda, platnum):
+        self.roda = roda
+        self._tipe = tipe
+        self.__name = name  # atribute private ditandai dengan dua garis bawah
+        self.__platnum = platnum # merupakan atribute private (hak akses classnya sendiri, tidak bisa diakses oleh luar kelas maupun turunannya)
+
+class Bebek:
+    def speak(self, bunyi_suara):
+        return bunyi_suara          #Nilai kembalian
+
+# Membuat objek dari kelas Bebek 
+donald = Bebek()
+
+# Memanggil Method Speak
+suara_donald = donald.speak("Kwek kwek")
+print(suara_donald)        
---
+++ 06_Object Oriented Programming/readme.md
@@ -0,0 +1,14 @@
+OOP (Object Oriented Programming)
+Pemrograman berorientasi objek adalah paradigma yang memungkinkan kita mengorganisir program sehingga perilaku dan properti di kelola oleh individu. 
+Properti ditentukan oleh nilai atribut, sedangkan perilaku akan ditentukan dari bagaimana objek tersebut bertindak atau reaksi terhadap permintaan. 
+
+Fundamental konsep OOP
+1. Encapsulation. Merupakan seni dari pembungkusan objek. Melibatkan pengelompokan atribut dan metode dalam satu unit yang dikenal dengan kelas.
+Basic encapsularion analogy: 
+- class (template/blueprint) berisikan beberapa atribut atau metode,
+- attributes, 
+- method.
+
+2. Data Abstraction. Adalah konsep dimana kita hanya menampilkan fitur-fitur penting dari suatu objek dan menyembunyikan detail kompleksnya. 
+3. Inheritance
+4. Polymorphism
